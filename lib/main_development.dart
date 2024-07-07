8998766d31f6172453003a75dad1847af800a317
Tarek Mohamed <tarekmohamed1019@gmail.com>
2024-07-07 10:47:55 +0300

    - added firebase to the project - create first app release from Production - fix errors appeared when adding firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,3 +1,5 @@
+import 'package:doc_doc/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -9,6 +11,10 @@ void main() async {
   setupGetIt();
   // To fix texts being hidden bug in flutter_screenutil in release mode.
   await ScreenUtil.ensureScreenSize();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     DocApp(
       appRouter: AppRouter(),
